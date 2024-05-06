2036b2bdd2ffc76b804a32d66bbd0ac47aabb721
yelo-o <flyordig@gmail.com>
2024-05-06 12:09:45 +0900

    root 페이지에 헤더박스, 잔액 박스, 카운트업애니메이션, 원화 추가

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 function Home() {
+  const loggedIn = {firstName: 'TomKim'}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and
+            transactions efficiently."
+          />
+          <TotalBalanceBox 
+            accounts={1}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
