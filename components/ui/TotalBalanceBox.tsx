0d16cfbd76e403a843d44d368957796ed740f428
yelo-o <flyordig@gmail.com>
2024-05-06 16:28:21 +0900

    chart.js 적용

--- components/ui/TotalBalanceBox.tsx
+++ components/ui/TotalBalanceBox.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
@@ -7,7 +8,7 @@ const TotalBalanceBox = ({
   return (
     <section className='total-balance'>
         <div className="total-balance-chart">
-            {/* 도넛 차트 */}
+            <DoughnutChart accounts={ accounts }/>
         </div>
 
         <div className="flex flex-col gap-6">
@@ -18,9 +19,9 @@ const TotalBalanceBox = ({
                 <p className='total-balance-label'>
                     Total Current Balance
                 </p>
-                <p className='total-balance-amount flex-center gap-2'>
+                <div className='total-balance-amount flex-center gap-2'>
                     <AnimatedCounter amount={totalCurrentBalance}/>
-                </p>
+                </div>
             </div>
         </div>
     </section>
