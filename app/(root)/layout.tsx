6a9a522b37b7d2c89649a8228d556912ab61d56c
yelo-o <flyordig@gmail.com>
2024-05-07 23:31:37 +0900

    데스크톱 사이드바 만들기

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import Sidebar from "@/components/ui/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Tom', lastName: 'Kim'};
+  
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
         {children}
     </main>
   );
