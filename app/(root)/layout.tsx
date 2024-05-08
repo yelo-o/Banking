c1b644acadac1d09e987b1ed021a3d6823eef365
yelo-o <flyordig@gmail.com>
2024-05-08 23:00:32 +0900

    모바일 navbar 추가 - shadcn에서 sheet 컴포넌트 이용

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/ui/MobileNav";
 import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,11 +8,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const loggedIn = { firstName: 'Tom', lastName: 'Kim'};
-  
+
   return (
     <main className="flex h-screen w-full font-inter">
         <Sidebar user={loggedIn} />
-        {children}
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+            <div>
+              <MobileNav user={loggedIn}/>
+            </div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
