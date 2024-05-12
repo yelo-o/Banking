cd86806949cefb22526aef96ba17788a5a9171f4
yelo-o <flyordig@gmail.com>
2024-05-12 18:59:01 +0900

    카드 컴포넌트 추가

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -27,7 +27,7 @@ function Home() {
       <RightSidebar 
         user={loggedIn}
         transactions={[]}
-        banks={[{},{}]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
       />
     </section>
   )
