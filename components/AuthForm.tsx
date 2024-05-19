cb8f018dc3edcf4f8cea93de794bd702047d9798
yelo-o <flyordig@gmail.com>
2024-05-19 17:25:07 +0900

    로딩 state 추가, sign-in, sign-up 페이지 이동 링크 추가

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,20 +20,18 @@ import {
 import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
-const formSchema = z.object({
-    email: z.string().email(),
-})
-  
+import { Loader2 } from 'lucide-react';
 
 const AuthForm = ({ type }: {type: string}) => {
     const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     // 1. Define your form.
     const form = useForm<z.infer<typeof authFormSchema>>({
     resolver: zodResolver(authFormSchema),
     defaultValues: {
       email: "",
-      password: '0'
+      password: ''
     },
   })
  
@@ -41,7 +39,9 @@ const AuthForm = ({ type }: {type: string}) => {
     function onSubmit(values: z.infer<typeof authFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
+    setIsLoading(true);
     console.log(values)
+    setIsLoading(false);
   }
   return (
     <section className="auth-form">
@@ -88,9 +88,25 @@ const AuthForm = ({ type }: {type: string}) => {
                         <CustomInput control={form.control} name='password' label='Password' 
                             placeholder='Enter your password' 
                         />
-                        <Button type="submit">Submit</Button>
+                        <div className='flex flex-col gap-4'>
+                            <Button type="submit" disabled={isLoading} className='form-btn'>
+                                {isLoading ? (
+                                    <>
+                                        <Loader2 size={20} className='animate-spin'/> &nbsp;
+                                        Loading...
+                                    </>
+                                ) : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+                            </Button>
+                        </div>
                     </form>
                 </Form>
+                <footer className='flex justify-center gap-1'>
+                    <p className='text-14 font-normal text-gray-600'>{type === 'sign-in' ? "Don't have an account?" : "Already have an account?"}</p>
+                    <Link href={type === 'sign-in' ? "/sign-up" : "/sign-in"} className='form-link'>
+                        {type === 'sign-in' ? "Sign Up" : "Sign In"}
+                    </Link>
+
+                </footer>
             </>
         )}
     </section>
